3d4c55dd31e76bc9e08068e6885eb9e034cc22d2
João Campanate <jvcampanate@gmail.com>
2026-07-28 01:41:02 -0300

    adicionarmateria_em_funcao
    Registrar matéria(primeira opção) agora é uma função chamada registrar_materia

--- functions/__init__.py
+++ functions/__init__.py
@@ -6,3 +6,18 @@ def inserir_materia():
     limite_faltas = leiaInt("Digite o limite de faltas da matéria: ")
     materia = Materia(nome_materia, 0, limite_faltas)
     return (materia)
+
+def materia_existe(cursor, materia):
+    cursor.execute(
+        "SELECT 1 FROM Materias WHERE nome_materia = ?;",
+        (materia)
+    )
+    return cursor.fetchone() is not None
+
+def registrar_materia(cursor):
+    materia = inserir_materia()
+    if not materia_existe(cursor, materia.nome):
+        cursor.execute("INSERT INTO Materias (nome_materia,qtd_faltas,lim_faltas) VALUES (?,?,?)",(materia.nome,materia.faltas,materia.lim_faltas))
+        print("Matéria adicionada!")
+    else:
+        print("A matéria já existe!")
--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from classes import *
 import sqlite3
 
 conexao = sqlite3.connect('data/faltas.db')
-cursor = conexao.cursor()
-cursor.execute('''CREATE TABLE IF NOT EXISTS Materias (
+cursor1 = conexao.cursor()
+cursor1.execute('''CREATE TABLE IF NOT EXISTS Materias (
 id INTEGER PRIMARY KEY,
 nome_materia TEXT NOT NULL,
 qtd_faltas INTEGER NOT NULL,
@@ -15,9 +15,7 @@ while True:
     opcao_escolhida = criarmenu("CONTROLE DE FALTAS V2")
     try:
         if opcao_escolhida == 1:
-            materia = inserir_materia()
-            cursor.execute("INSERT INTO Materias (nome_materia,qtd_faltas,lim_faltas) VALUES (?,?,?)",(materia.nome,materia.faltas,materia.lim_faltas))
-            print("Matéria adicionada!")
+            registrar_materia(cursor=cursor1)
 
         elif opcao_escolhida == 2:
             continue
