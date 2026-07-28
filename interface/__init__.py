422e3513f98eb18d4d99d8434529a3332696df18
João Campanate <jvcampanate@gmail.com>
2026-07-28 00:09:42 -0300

    primeiras_funcoes_interface
    adição das primeiras funções no módulo da interface

---
+++ interface/__init__.py
@@ -0,0 +1,51 @@
+from time import sleep
+
+def cabecalho(txt):
+
+    print('-='*30)
+    print(f'{txt:^40}')
+    print('-='*30)
+
+def leiaInt(msg = ''):
+
+    while True:
+        n = str(input(msg))
+        try:
+            int(n)
+        except:
+            print('Valor inválido.')
+            continue
+        else:
+            return int(n)
+
+def criarmenu(txt):
+
+    cabecalho(txt)
+    sleep(1)
+    cont = 0
+
+    opcoes = ['Adicionar matéria', 'Registrar falta','Ver faltas', 'Alterar matéria', 'Apagar todos os dados', 'Sair do programa']
+
+    for e in opcoes():
+        cont +=1
+        print(f'{cont} - {e}')
+        sleep(0.5)
+
+    print('-'*20)
+    opcao = leiaInt('Opção selecionada: ')
+    print('-'*20)
+    sleep(0.5)
+
+    while opcao not in range(1, cont+1):
+        opcao = leiaInt('Opção inválida. Digite novamente: ')
+
+    return opcao
+
+def confirmarResposta(msg):
+    resposta = str(input(msg)).upper().strip()
+    while resposta != 'S' and resposta != 'N':
+        resposta = str(input('Resposta inválida. Tente novamente: ')).upper().strip()
+    if resposta == 'S':
+        return True
+    else:
+        return False
--- main.py
+++ main.py
@@ -3,3 +3,5 @@ from interface import *
 import sqlite3
 
 conexao = sqlite3.connect('data/faltas.db')
+cursor = conexao.cursor()
+
