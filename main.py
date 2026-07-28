a64f0552457639b96f965f9b37c2cd6035200b5d
João Campanate <jvcampanate@gmail.com>
2026-07-28 02:09:46 -0300

    organizacao_arquivo_principal
    Mudei o modo de importar os outros modulos, para que fique mais claro de qual modulo vem cada função que estou usando.

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-from functions import *
-from interface import *
-from classes import *
+import functions
+import interface
 import sqlite3
 
 conexao1 = sqlite3.connect('data/faltas.db')
 cursor1 = conexao1.cursor()
-criar_tabela_materias(cursor=cursor1)
+functions.criar_tabela_materias(cursor=cursor1)
 conexao1.commit()
 
 while True:
-    opcao_escolhida = criarmenu("CONTROLE DE FALTAS V2")
+    opcao_escolhida = interface.criarmenu("CONTROLE DE FALTAS V2")
     try:
         if opcao_escolhida == 1:
-            registrar_materia(cursor=cursor1)
+            functions.registrar_materia(cursor=cursor1)
 
         elif opcao_escolhida == 2:
             continue
